cc227820330368213e5e5cd887c28395e8c948f1
Elkyn E <efenriquezm@gmail.com>
2016-12-11 14:53:27 -0500

    index
    principal

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>I.E. Francisco de la Villota</title>
+        <meta charset="UTF-8">
+        <?php include "php/links.html" ?>
+    </head>
+        
+    <body>
+<!-- ************************************************************ -->
+       <!--CABECERA PRINCIPAL - MENU GENERAL-->
+       <div class="parallax-container">       
+       <div class="parallax"><img src="img/Portada_1.jpg"> </div>
+       <!--CABECERA PRINCIPAL - INCLUSION DE MENU-->
+       <?php include "php/menu_principal.php" ?>
+       </div>
+<!-- ************************************************************ -->
+      <!--PRIMERA SESION TEXTO ALTERNATIVO I.E-->
+      <div class="superior aoi"></div>
+       <div class="section beach">
+          <div class="row container">
+              <h4 class="header center"><FONT FACE="arial black">I.E.M Francisco de la Villota</FONT></h4> <hr>
+              <p align="justify" class="flow-text">La Institución Educativa Municipal Francisco de la Villota, es una comunidad educativa de personas unidas por objetivos comunes, lo cual impone responsabilidades y derechos recíprocos.</p> <hr>
+             <!-- Modal Trigger -->
+  <a class="modal-trigger waves-effect waves-light aoi btn" href="#modal1">Leer Mas</a>
+
+  <!-- Modal Structure -->
+  <div id="modal1" class="modal modal-fixed-footer">
+    <div class="modal-content">
+      <h4 class="center">Mas que solo Educar!</h4> <hr>
+      <p class="flow-text center"><img src="img/francisco2.png" height="125px" class="center"> <br> La actuación de los miembros de la comunidad académica se orienta dentro de unas relaciones armónicas fundamentadas en el respecto, así mismo , al otro y a su entorno, la tolerancia y la solidaridad. Dentro de su cultura organizacional promueve la calidad, entendida como una cualidad que permite evaluar, los procesos, métodos, instrumentos y logros de la institución de acuerdo con estándares e indicadores internos y externos, posibilitando el mejoramiento continuo que exige una permanente dinámica de formación académica, investigativa e innovadora para afrontar las incertidumbres del mundo de hoy.los mas de 750 niños todos pertenecientes a la comunidad de genoy cuentan con esta gran insitucion para su superación personal.</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect aoi btn-flat ">CERRAR</a>
+    </div>
+  </div>
+          
+          </div>   
+       </div>
+       <div class="superior aoi"></div>
+<!-- ************************************************************ -->    
+      <!--PARALLAX IMG--> 
+       <div class="parallax-container">
+        <div class="parallax"><img src="img/IMG_1655.jpg"></div>
+      </div>
+<!-- ************************************************************ -->
+      <!--SEGUNA SESION TARJETAS NOTICIAS--> 
+      <div class="superior aoi"></div>
+     <div class="section beach">
+         <div class="row ">
+             <div class="carousel">
+             <!-- UNO - maraton -->
+              <div class="card small col l4 m6 s10 aoi carousel-item">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="img/maraton.png">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator black-text">Maraton<i class="material-icons right">more_vert</i></span>
+                  <p><a href="#" class="black-text">Primera maratón de Enseñanza</a></p>
+                </div>
+                <div class="card-reveal clean">
+                  <span class="card-title clean">Maraton de Enseñanza<i class="material-icons right">close</i></span>
+                  <p>¡GRACIAS!Participantes de la I Maratón de Enseñanza utilizando las TIC</p>
+                </div>
+              </div>
+             <!-- UNO -->
+              <div class="card small col l4 m6 s10 aoi carousel-item">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="img/IMG_2046.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator black-text">Estudiantes<i class="material-icons right">more_vert</i></span>
+                  <p><a href="#" class="black-text">Aprendizaje con Ambición </a></p>
+                </div>
+                <div class="card-reveal clean">
+                  <span class="card-title clean">Futuro, ambición y aprendizaje<i class="material-icons right">close</i></span>
+                  <p>Es mejor enseñar desde el punto donde el niño juegue aprendiendo</p>
+                </div>
+              </div>
+              <!-- DOS -->
+                <div class="card small col l4 m6 s10 aoi carousel-item">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="img/IMG_1878.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator black-text">Educación<i class="material-icons right">more_vert</i></span>
+                  <p><a href="#" class="black-text">Educación para el Futuro</a></p>
+                </div>
+                <div class="card-reveal clean">
+                  <span class="card-title clean">Nuestra Educacion<i class="material-icons right">close</i></span>
+                  <p>Es mejor enseñar desde el punto donde el niño juegue aprendiendo</p>
+                </div>
+              </div>
+              <!-- TRES -->
+                <div class="card small col l4 m6 s10 aoi carousel-item">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="img/IMG_2001.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator black-text">Arte y Dimención<i class="material-icons right">more_vert</i></span>
+                  <p><a href="#" class="black-text">Nuestra institución nuestra cultura</a></p>
+                </div>
+                <div class="card-reveal clean">
+                  <span class="card-title clean">Carnaval Estudiantil<i class="material-icons right">close</i></span>
+                  <p>El Arte es el mejor regalo, la dirvesion es la mejor forma de aprender</p>
+                </div>
+              </div>
+              <!-- CUATRO -->
+                <div class="card small col l4 m6 s10 aoi carousel-item">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="img/IMG_1912.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator black-text">Aprendizaje<i class="material-icons right">more_vert</i></span>
+                  <p><a href="#" class="black-text">Construir Conocimiento, labor de todos</a></p>
+                </div>
+                <div class="card-reveal clean">
+                  <span class="card-title clean">Nuestra Educacion<i class="material-icons right">close</i></span>
+                  <p>La involucración de todos, como gesto para la construción del conocimineto colectivo.</p>
+                </div>
+              </div>
+              <!-- CINCO -->
+                <div class="card small col l4 m6 s10 aoi carousel-item">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="img/IMG_2086.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator black-text">Docencia<i class="material-icons right">more_vert</i></span>
+                  <p><a href="#" class="black-text">Enseñar es un Arte!</a></p>
+                </div>
+                <div class="card-reveal clean">
+                  <span class="card-title clean">Mucho mas que Enseñar!<i class="material-icons right">close</i></span>
+                  <p>“En la realidad actual la escuela tiene que enseñar mucho más que a leer y escribir –explica Straccia-. Por ejemplo la construcción y el análisis de imágenes, la construcción de un video, la posibilidad de alfabetizar en otros campos. Hoy lo chicos de dos años ya utilizan celulares y eso les permite tener una formación distinta.”</p>
+                </div>
+              </div>
+              <!-- FIN -->
+             </div>
+          </div> 
+    </div>
+    <div class="superior aoi"></div>
+<!-- ************************************************************ -->
+      <!--TERCERA SESION MULTIMEDIA SLIDER...?--> 
+    <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="img/IMG_1826.jpg" class="responsive-img">
+      </li>
+      <li>
+        <img src="img/IMG_1925-1.jpg">     
+      </li>
+      <li>
+        <img src="img/IMG_1638.jpg">             
+      </li>
+      <li>
+        <img src="img/IMG_1661-1.jpg"> 
+        
+      </li>
+    </ul>
+  </div>
+
+  <div class="superior orange"></div>
+       <div class="section unreal">         
+          <div class="row container center">
+              <h5 class="header center"><font FACE="arial black">Visitanos,<br> nos encontramos en el Corregimiento de Genoy, Nariño.</font></h5> <hr>  
+          </div> 
+           <iframe src="https://www.google.com/maps/d/embed?mid=1Ftf69IEl012DqtAiSvpq-MnC_DA&hl=en_US" width="100%" height="350"></iframe>  
+       </div>
+
+<!-- ************************************************************ -->    
+    <!-- PIE DE PAGINA -->    
+          
+    <div class="superior orange"></div>
+    <footer class="footer black">
+       <?php include "php/pie-pagina.php" ?> 
+    </footer>                    
+<!-- ************************************************************ -->
+        <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/inicializacion.js"></script>
+      <script>
+        //SLIDER
+        $(document).ready(function(){
+            $('.slider').slider({full_width: true});
+        });
+        //PARALLAX
+        $(document).ready(function(){
+            $('.parallax').parallax();
+        });
+        //CARROUSEL
+        $(document).ready(function(){        
+            $('.carousel').carousel(); 
+        });
+          
+        $(document).ready(function(){
+            $('.carousel').carousel({dist:0});
+            window.setInterval(function(){$('.carousel').carousel('next')},7500)
+        });
+          
+        //Modale
+        $(document).ready(function(){
+            $('.modal').modal();
+        });
+      </script>
+        <!-- Fin importacion de jQuery-->
+<!-- ************************************************************ -->
+    </body>   
+</html>
