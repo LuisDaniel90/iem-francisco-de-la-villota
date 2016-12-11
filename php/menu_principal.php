9835df656e82ae084b176ea3f4068a5129e73a37
Elkyn E <efenriquezm@gmail.com>
2016-12-11 15:00:30 -0500

    Php

---
+++ php/menu_principal.php
@@ -0,0 +1,125 @@
+<!-- ESTRUCTURA SUBMENU PARA: NUESTRA INSTITUCION -->
+<ul id="institucion1" class="dropdown-content ">
+  <li><a href="mision.php" class="white-text center">MSION</a></li>
+  <li class="divider"></li>
+  <li><a href="vision.php" class="white-text center">VISION</a></li>
+  <li class="divider"></li>
+  <li><a href="pei.php" class="white-text center">PEI</a></li>
+  <li class="divider"></li>
+  <li><a href="pet.php" class="white-text center">PET</a></li>
+</ul>  
+
+<ul id="institucion2" class="dropdown-content dropdown-content-movil">
+  <li><a href="mision.php" class="black-text center">MSION</a></li>
+  <li class="divider"></li>
+  <li><a href="vision.php" class="black-text center">VISION</a></li>
+  <li class="divider"></li>
+  <li><a href="pei.php" class="black-text center">PEI</a></li>
+  <li class="divider"></li>
+  <li><a href="pet.php" class="black-text center">PET</a></li>
+</ul>  
+ 
+  <!-- ESTRUCTURA SUBMENU PARA: ACADEMIA -->
+<ul id="academia1" class="dropdown-content">
+  <li><a href="preescolar.php" class="white-text center">PREESCOLAR</a></li>
+  <li class="divider"></li>
+  <li><a href="primaria.php" class="white-text center">PRIMARIA</a></li>
+  <li class="divider"></li>
+  <li><a href="bachillerato.php" class="white-text center">BACHILLERATO</a></li>
+</ul>  
+ 
+ <ul id="academia2" class="dropdown-content dropdown-content-movil">
+  <li><a href="preescolar.php" class="black-text center">PREESCOLAR</a></li>
+  <li class="divider"></li>
+  <li><a href="primaria.php" class="black-text center">PRIMARIA</a></li>
+  <li class="divider"></li>
+  <li><a href="bachillerato.php" class="black-text center">BACHILLERATO</a></li>
+</ul> 
+  
+   <!-- ESTRUCTURA SUBMENU PARA: GESTION -->
+<ul id="gestion1" class="dropdown-content">
+  <li><a href="directiva.php" class="white-text center">GESTION DIRECTIVA</a></li>
+  <li class="divider"></li>
+  <li><a href="academica.php" class="white-text center">GESTION ACADEMICA</a></li>
+  <li class="divider"></li>
+  <li><a href="administrativa.php" class="white-text center">GESTION ADMINISTRATIVA Y FINANCIERA</a></li>
+  <li class="divider"></li>
+  <li><a href="comunitaria.php" class="white-text center">GESTION COMUNITARIA</a></li>
+</ul>
+
+ <ul id="gestion2" class="dropdown-content dropdown-content-movil">
+  <li><a href="directiva.php" class="black-text center">GESTION DIRECTIVA</a></li>
+  <li class="divider"></li>
+  <li><a href="academica.php" class="black-text center">GESTION ACADEMICA</a></li>
+  <li class="divider"></li>
+  <li><a href="administrativa.php" class="black-text center">GESTION ADMINISTRATIVA</a></li>
+  <li class="divider"></li>
+  <li><a href="comunitaria.php" class="black-text center">GESTION COMUNITARIA</a></li>
+</ul>
+  
+  
+<!-- ESTRUCTURA MENU GENERAL -->
+  <div class="navbar-fixed">
+   <nav>
+    <div class="nav-wrapper contenedor-menu">
+        <a href="index.php" class="brand-logo black-text"> <img src="img/francisco2.png" height="60px" class="escudo"><i class="flow-text right titulo"> I.E.M Francisco de la Villota</i></a>
+     
+       <a href="#" data-activates="mobile-demo" class="button-collapse flow-text left large"><i class="material-icons ">menu</i></a>
+       
+<!-- ESTRUCTURA MENU GENERAL COMPUTADORAS--> 
+       <ul class="right hide-on-med-and-down">
+        <li><a href="index.php" class="center"> <i class="left icon-home flow-text"></i>Inicio</a></li>
+        <li><a href="#" class="center dropdown-button" data-activates="institucion1"><i class="left icon-insitucion flow-text"></i> Nuestra Institucion</a></li>
+        <li><a href="#" class="center dropdown-button" data-activates="academia1"><i class="left icon-academia flow-text"></i>Academia</a></li>
+        <li><a href="#" class="center dropdown-button" data-activates="gestion1"><i class="left icon-gestion flow-text"></i>Áreas de Gestion</a></li>
+      </ul>
+      
+<!-- ESTRUCTURA MENU GENERAL MOVIL -->
+       <ul class="side-nav collapsible" id="mobile-demo" data-collapsible="accordion">
+       
+        <li>
+        <div class="collapsible-header">
+        <a href="index.php" class="white-text">
+        <i class="left icon-home white-text"></i>Inicio</a>
+        </div>
+        </li>
+        
+        <li class="divider"></li>
+        
+        <li>
+        <div class="collapsible-header">
+        <a href="#" class="white-text dropdown-button" data-activates="institucion2">
+        <i class="left icon-insitucion white-text"></i>Nuestra Institución</a>
+        </div>
+        </li>
+        
+        <li class="divider"></li>
+        
+        <li>
+        <div class="collapsible-header">
+        <a href="#" class="white-text dropdown-button" data-activates="academia2">
+        <i class="left icon-academia white-text"></i>Academia</a>
+        </div>
+        </li>
+        
+        <li class="divider"></li>
+        
+        <li>
+        
+        <div class="collapsible-header">
+        <a href="#" class="white-text dropdown-button" data-activates="gestion2"> 
+        <i class="left icon-gestion white-text"></i>Áreas de Gestión</a> 
+        </div>
+        </li>
+        
+        <li class="divider"></li>
+        
+        <li class="center"> <br>
+        <h6 class="flow-text">I.E.M Francisco de la Villota</h6>
+        <img src="img/francisco2.png" width="50%" >
+        </li>
+        
+      </ul>
+    </div>
+  </nav>
+</div>
---
+++ php/pie-pagina.php
@@ -0,0 +1,23 @@
+ <div class="row">
+            <div class="col l4 m4 s12">
+                <h5>Contacto </h5><hr>
+                Corregimiento de Genoy <br>
+                San Juan de Pasto Nariño <br>
+                317 842 6976 <br>
+                iemfranciscodelavillota@gmail.com
+                
+            </div>
+            <div class="col l4 m4 s12 ">
+                <img src="img/francisco2.png" height="90px"> <hr>
+                 <p class="copy">&copy; I.E.M Francisco de la Villota - 2016 <br> Todos los derechos reservados</p>
+            </div>
+            <div class="col l4 m4 s12">
+               <h5>Siguenos</h5> <hr>
+                <div class="social">
+            <a href="#" target="_blank" class="icon-facebook"></a>
+            <a href="#" target="_blank" class="icon-twitter"></a>
+            <a href="#" target="_blank" class="icon-youtube"></a>
+            <a href="#" target="_blank" class="icon-google"></a>
+                </div>
+            </div>
+        </div>
