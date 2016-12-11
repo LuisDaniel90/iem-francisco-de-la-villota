fe367ceab7ed278f11e37f1c97803ab70a8ab40f
Elkyn E <efenriquezm@gmail.com>
2016-12-11 14:59:24 -0500

    Javascript

---
+++ js/mis_scripts.js
@@ -0,0 +1,18 @@
+ 
+/*SCRIPT DEL PARALLAX*/
+ $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+/*SCRIPT DEL SLIDER*/
+  $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+  /*SCRIPT DEL DESLIZABLE*/
+   $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+ /*SCRIPT DE CHIP*/
+ $('.chips-initial').material_chip('data');
+ /*SCRIPT MENU RESPONSIVE*/
+  $(".button-collapse").sideNav();
